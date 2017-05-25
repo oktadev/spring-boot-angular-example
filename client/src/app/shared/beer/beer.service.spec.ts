627ff6e3901d44f138feeb2ea88fd841a989254a
Matt Raible <matt@raibledesigns.com>
2017-05-25 08:38:26 -0600

    Fix tests and add Travis

--- client/src/app/shared/beer/beer.service.spec.ts
+++ client/src/app/shared/beer/beer.service.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { BeerService } from './beer.service';
+import { BaseRequestOptions, ConnectionBackend, Http } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 describe('BeerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BeerService]
+      providers: [BeerService,
+        {
+          provide: Http, useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+          return new Http(backend, defaultOptions);
+        },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        {provide: MockBackend, useClass: MockBackend},
+        {provide: BaseRequestOptions, useClass: BaseRequestOptions}]
     });
   });
 
